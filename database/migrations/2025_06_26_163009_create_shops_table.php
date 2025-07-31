17a9bc9781a182b461253467e6295022a298bb55
softongr <softongr@gmail.com>
2025-07-31 12:43:06 +0300

    Update 2025_06_26_163009_create_shops_table.php

--- database/migrations/2025_06_26_163009_create_shops_table.php
+++ database/migrations/2025_06_26_163009_create_shops_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name')->nullable()->index();
             $table->timestamps();
 
-            $table->index('name');
+          //  $table->index('name');
         });
     }
 
